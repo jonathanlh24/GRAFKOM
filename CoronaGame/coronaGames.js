e5d562366b763b03fba3314d550cf9cc3a3bdff2
jonathanlh24 <jonathanleonardihalim001@gmail.com>
2021-06-18 09:26:14 +0700

    update-baru

--- CoronaGame/coronaGames.js
+++ CoronaGame/coronaGames.js
@@ -5,15 +5,14 @@ let renderer = new THREE.WebGLRenderer({
     antialias: true
 });
 
-scene.background = new THREE.Color('0xfafafa');
+scene.background = new THREE.Color(0x000000);
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
 
-cam.position.z = 25;
-cam.position.y = 10;
+cam.position.z = 30;
+cam.position.y = 15;
 
 const color = new THREE.Color(0x00ff00);
-
 
 let virus_loader1 = new THREE.GLTFLoader();
 virus_loader1.load("Virus-1/scene.gltf", function (gltf) {
@@ -60,7 +59,7 @@ let controls = new THREE.OrbitControls(cam, renderer.domElement);
 
 const plane = new THREE.PlaneGeometry(30, 30, 1);
 const checkpoint = new THREE.PlaneGeometry(30, 5, 5);
-const tanah = new THREE.BoxGeometry(30,30,30);
+const tanah = new THREE.BoxGeometry(30, 30, 30);
 
 const grass_texture = new THREE.TextureLoader().load('./texture/rumput.jpg');
 const tanah_texture = new THREE.TextureLoader().load('./texture/tanah.jpg');
@@ -78,7 +77,7 @@ const mat_rumput = new THREE.MeshPhongMaterial({
 });
 
 let mesh_checkpoint = new THREE.Mesh(checkpoint, mat_checkpoint);
-mesh_checkpoint.position.set(0, 0.05, -15);
+mesh_checkpoint.position.set(0, 0.05, -12.5);
 scene.add(mesh_checkpoint);
 mesh_checkpoint.rotation.x -= Math.PI / 2;
 
@@ -132,52 +131,56 @@ light8.position.set(-15, -15, -15);
 scene.add(light8);
 scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
 
-// let light9 = new THREE.PointLight(0xffffff, 0.5); //front center
-// light8.position.set(0, 0, 15);
-// scene.add(light8);
-// scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
+let light9 = new THREE.PointLight(0xffffff, 0.5); //front center
+light9.position.set(0, 0, 30);
+scene.add(light9);
+scene.add(new THREE.PointLightHelper(light9, 1.0, 0x0000ff));
 
-// let light10 = new THREE.PointLight(0xffffff, 0.5); //back center
-// light8.position.set(0, 0, -15);
-// scene.add(light8);
-// scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
+let light10 = new THREE.PointLight(0xffffff, 0.5); //back center
+light10.position.set(0, 0, -30);
+scene.add(light10);
+scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
 
-// let light8 = new THREE.PointLight(0xffffff, 0.5);
-// light8.position.set(-15, -15, -15);
-// scene.add(light8);
-// scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
+let light11 = new THREE.PointLight(0xffffff, 0.5); //left
+light11.position.set(-30, 0, 0);
+scene.add(light11);
+scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
 
-// let light8 = new THREE.PointLight(0xffffff, 0.5);
-// light8.position.set(-15, -15, -15);
-// scene.add(light8);
-// scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
+let light12 = new THREE.PointLight(0xffffff, 0.5); //right
+light12.position.set(30, 0, 0);
+scene.add(light12);
+scene.add(new THREE.PointLightHelper(light8, 1.0, 0x0000ff));
 
+var person = new THREE.Object3D();
+var virus_1 = new THREE.Object3D();
+var virus_2 = new THREE.Object3D();
+var virus_3 = new THREE.Object3D();
+var virus_4 = new THREE.Object3D();
 
 // Controller
-let posisi = 'depan'
 document.addEventListener('keydown', function (event) {
     // ATAS = W
     if (event.keyCode == 87) {
         console.log("W");
-        person.position.z -= 0.5;
+        person.position.z -= 1;
     }
 
     // KIRI = A
     else if (event.keyCode == 65) {
         console.log("A");
-        person.position.x -= 0.5;
+        person.position.x -= 1;
     }
 
     // BAWAH = S
     else if (event.keyCode == 83) {
         console.log("S");
-        person.position.z += 0.5;
+        person.position.z += 1;
     }
 
     // KANAN = D
     else if (event.keyCode == 68) {
         console.log("D");
-        person.position.x += 0.5;
+        person.position.x += 1;
     }
 
     // ROTATE KIRI = Q
@@ -199,69 +202,79 @@ let checker3 = "kanan";
 let checker4 = "kanan";
 
 function draw() {
-    requestAnimationFrame(draw);
-    renderer.render(scene, cam);
-    
-    let posisi = virus_1.position.x;
-    if (checker == "kanan") {
-        if (posisi > 10) {
-            checker = "kiri";
-        } else {
-            virus_1.position.x += 0.1;
-        }
-    } else if (checker == "kiri") {
-        if (posisi < 5) {
-            checker = "kanan";
-        } else {
-            virus_1.position.x -= 0.1;
-        }
+
+    if (person.position.x > 15 || person.position.x < -15 || person.position.z > 15 || person.position.z < -15) {
+        alert("GAME OVER");
+        location.reload();
+    } else if (person.position.z <= -11) {
+        alert("CHECK POINT")
+        location.reload();
     }
+    else {
+        requestAnimationFrame(draw);
+        renderer.render(scene, cam);
 
-    // let posisi2 = virus_2.position.x;
-    // if (checker2 == "kanan") {
-    //     if (posisi2 > 10) {
-    //         checker2 = "kiri";
-    //     } else {
-    //         virus_2.position.x += 0.1;
-    //     }
-    // } else if (checker == "kiri") {
-    //     if (posisi2 < 5) {
-    //         checker2 = "kanan";
-    //     } else {
-    //         virus_2.position.x -= 0.1;
-    //     }
-    // }
+        let posisi = virus_1.position.x;
+        if (checker == "kanan") {
+            if (posisi > 10) {
+                checker = "kiri";
+            } else {
+                virus_1.position.x += 0.2;
+            }
+        } else if (checker == "kiri") {
+            if (posisi < -10) {
+                checker = "kanan";
+            } else {
+                virus_1.position.x -= 0.2;
+            }
+        }
 
-    // let posisi3 = virus_3.position.x;
-    // if (checker3 == "kanan") {
-    //     if (posisi3 > 10) {
-    //         checker3 = "kiri";
-    //     } else {
-    //         virus_3.position.x += 0.1;
-    //     }
-    // } else if (checker3 == "kiri") {
-    //     if (posisi3 < 5) {
-    //         checker3 = "kanan";
-    //     } else {
-    //         virus_3.position.x -= 0.1;
-    //     }
-    // }
+        let posisi2 = virus_2.position.x;
+        if (checker2 == "kanan") {
+            if (posisi2 > 10) {
+                checker2 = "kiri";
+            } else {
+                virus_2.position.x += 0.3;
+            }
+        } else if (checker2 == "kiri") {
+            if (posisi2 < -10) {
+                checker2 = "kanan";
+            } else {
+                virus_2.position.x -= 0.3;
+            }
+        }
 
-    // let posisi4 = virus_4.position.x;
-    // if (checker4 == "kanan") {
-    //     if (posisi4 > 10) {
-    //         checker4 = "kiri";
-    //     } else {
-    //         virus_4.position.x += 0.1;
-    //     }
-    // } else if (checker4 == "kiri") {
-    //     if (posisi4 < 5) {
-    //         checker4 = "kanan";
-    //     } else {
-    //         virus_4.position.x -= 0.1;
-    //     }
-    // }
-    console.log(checker)
+        let posisi3 = virus_3.position.x;
+        if (checker3 == "kanan") {
+            if (posisi3 > 10) {
+                checker3 = "kiri";
+            } else {
+                virus_3.position.x += 0.4;
+            }
+        } else if (checker3 == "kiri") {
+            if (posisi3 < -10) {
+                checker3 = "kanan";
+            } else {
+                virus_3.position.x -= 0.4;
+            }
+        }
+
+        let posisi4 = virus_4.position.x;
+        if (checker4 == "kanan") {
+            if (posisi4 > 10) {
+                checker4 = "kiri";
+            } else {
+                virus_4.position.x += 0.5;
+            }
+        } else if (checker4 == "kiri") {
+            if (posisi4 < -10) {
+                checker4 = "kanan";
+            } else {
+                virus_4.position.x -= 0.5;
+            }
+        }
+        console.log(checker)
+    }
 }
 
 draw();
